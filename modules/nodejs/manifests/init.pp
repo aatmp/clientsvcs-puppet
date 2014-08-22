c66c8992db1b9da9b5423bcdcf49f2ee25c4eca5
Adam Ravid <aravid@mediaplatform.com>
2014-08-22 08:45:34 -0700

    added el::epel

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -4,6 +4,7 @@ class nodejs::nodejs_common {
     case $lsbdistid {
 
         'CentOS', 'RedHat', 'RedHatEnterpriseServer': {
+	    include "el::epel"
             package {
                 "nodejs":		ensure  => latest,
                 			require => package["epel"]
